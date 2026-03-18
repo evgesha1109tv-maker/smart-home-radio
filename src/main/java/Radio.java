cb2bee17879ad4047afe85afb2b74c93ef3dc56f
Evgen P <evgesha1109tv@gmail.com>
2026-03-18 20:50:18 +0400

    lombok changes

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,5 +1,11 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
 public class Radio {
+    @Setter
     private int currentRadioStation;
+
     private int currentVolume;
     private int maxStation;
     private int countStation;
@@ -16,14 +22,14 @@ public class Radio {
         this.maxStation = countStation - 1;
     }
 
-//    public Radio(int currentRadioStation, int currentVolume, int maxStation) {
-//        this.currentRadioStation = currentRadioStation;
-//        this.currentVolume = currentVolume;
-//        this.maxStation = maxStation;
-//    }
-
-    public int getCurrentRadioStation() {
-        return currentRadioStation;
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 100) {
+            return;
+        }
+        this.currentVolume = currentVolume;
     }
 
     public void setCurrentRadioStation(int currentRadioStation) {
@@ -35,25 +41,6 @@ public class Radio {
         }
         this.currentRadioStation = currentRadioStation;
     }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume < 0) {
-            return;
-        }
-        if (currentVolume > 100) {
-            return;
-        }
-        this.currentVolume = currentVolume;
-    }
-
-    public int getMaxStation() {
-        return maxStation;
-    }
-
 
     public void increaseRadioStation() {
         if (currentRadioStation != maxStation) {
@@ -75,7 +62,6 @@ public class Radio {
         if (currentVolume != 100) {
             currentVolume++;
         }
-
     }
 
     public void reducingVolume() {
@@ -84,82 +70,3 @@ public class Radio {
         }
     }
 }
-
-
-//    public Radio() {
-//        this.maxStation = 9;
-//    }
-//
-//    public Radio(int countStation) {
-//        if (countStation<1){
-//            countStation=10;
-//        }
-//        this.maxStation = countStation - 1;
-//    }
-//
-//    public int getMaxStation() {
-//        return maxStation;
-//    }
-//
-//    public void setMaxStation(int maxStation) {
-//
-//    }
-//
-//    public int getCurrentRadioStation() {
-//        return currentRadioStation;
-//    }
-//
-//    public void setCurrentRadioStation(int newCurrentRadioStation) {
-//        if (newCurrentRadioStation < 0) {
-//            return;
-//        }
-//        if (newCurrentRadioStation > maxStation) {
-//            return;
-//        }
-//        currentRadioStation = newCurrentRadioStation;
-//    }
-//
-//    public int getCurrentVolume() {
-//        return currentVolume;
-//    }
-//
-//    public void setCurrentVolume(int newCurrentVolume) {
-//        if (newCurrentVolume < 0) {
-//            return;
-//        }
-//        if (newCurrentVolume > 100) {
-//            return;
-//        }
-//        currentVolume = newCurrentVolume;
-//    }
-//
-//    public void increaseRadioStation() {
-//        if (currentRadioStation != maxStation) {
-//            currentRadioStation++;
-//            return;
-//        }
-//        currentRadioStation = 0;
-//    }
-//
-//    public void reducingRadioStation() {
-//        if (currentRadioStation != 0) {
-//            currentRadioStation--;
-//            return;
-//        }
-//        currentRadioStation = maxStation;
-//    }
-//
-//    public void increaseVolume() {
-//        if (currentVolume != 100) {
-//            currentVolume++;
-//        }
-//
-//    }
-//
-//    public void reducingVolume() {
-//        if (currentVolume != 0) {
-//            currentVolume--;
-//        }
-//    }
-//
-//}
